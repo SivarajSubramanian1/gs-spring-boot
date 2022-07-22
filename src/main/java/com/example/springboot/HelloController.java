495a2ead302ea07067c361c6af8524727c6d2011
Sivaraj Subramanian <sivaraj@Sivarajs-Mac-mini.local>
2022-07-22 19:44:12 +1000

    Added new end points

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -11,6 +11,29 @@ public class HelloController {
 		return "Greetings from Spring Boot!";
 	}
 
+	@GetMapping("/api/v1/team")
+	public String index() {
+		return "Greetings from Team!";
+	}
+	@GetMapping("/api/v1/buy")
+	public String index() {
+		return "Greetings from Buy!";
+	}
+	@GetMapping("/api/v1/sell")
+	public String index() {
+		return "Greetings from sell!";
+	}
+
+	@GetMapping("/api/v1/sell/source")
+	public String index() {
+		return "Greetings from source sell!";
+	}
+
+	@GetMapping("/api/v1/sell/2")
+	public String index() {
+		return "Greetings from sell2!";
+	}
+
 	@GetMapping("/api/v0")
 	public UserResponse restResponse() {
 		UserResponse userResponse = new UserResponse();
