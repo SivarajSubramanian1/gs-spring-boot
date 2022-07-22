4a1ab6d71db69a3f362527b5695acbe47837915e
Sivaraj Subramanian <sivaraj@Sivarajs-Mac-mini.local>
2022-07-22 13:42:21 +1000

    Added a html index

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -6,9 +6,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
+	@GetMapping("/hello")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
 
+	@GetMapping("/api/v0")
+	public UserResponse restResponse() {
+		UserResponse userResponse = new UserResponse();
+
+		userResponse.setStatus(true);
+		userResponse.setMessage("Data is found");
+
+		return userResponse;
+	}
+
 }
+
---
+++ src/main/java/com/example/springboot/UserResponse.java
@@ -0,0 +1,28 @@
+package com.example.springboot;
+
+public class UserResponse {
+    private Boolean status;
+    private String message;
+
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public UserResponse(){
+    }
+
+    // setters & getters
+}
