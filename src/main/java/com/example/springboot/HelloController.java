537bf61e3cc18d71354e00cd465b693cfa778db8
Sivaraj Subramanian <sivaraj@Sivarajs-Mac-mini.local>
2022-07-25 09:49:27 +1000

    Added method for tracking

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -4,64 +4,73 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.lang.*;
+
 @RestController
 public class HelloController {
 
-	@GetMapping("/hello")
-	public String index() {
-		return "Greetings from Spring Boot!";
-	}
+    @GetMapping("/hello")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
 
-	@GetMapping("/api/v1/team")
-	public String team() {
-		return "Greetings from Team!";
-	}
-	@GetMapping("/api/v1/buy")
-	public String buy() {
-		return "Greetings from Buy!";
-	}
-	@GetMapping("/api/v1/sell")
-	public String sell() {
-		checkOutvalue(230);
-		return "Greetings from sell!";
-	}
+    @GetMapping("/api/v1/team")
+    public String team() {
+        return "Greetings from Team!";
+    }
 
-	@GetMapping("/api/v1/error2")
-	public ResponseEntity<String> error401() {
-		return new ResponseEntity(HttpStatus.BAD_REQUEST);
-	}
+    @GetMapping("/api/v1/buy")
+    public String buy() {
+        checkOutvalue2(300);
+        return "Greetings from Buy!";
+    }
+
+    @GetMapping("/api/v1/sell")
+    public String sell() {
+        checkOutvalue(230);
+        return "Greetings from sell!";
+    }
+
+    @GetMapping("/api/v1/error2")
+    public ResponseEntity<String> error401() {
+        return new ResponseEntity(HttpStatus.BAD_REQUEST);
+    }
 
 
-	@GetMapping("/api/v1/sell/source")
-	public String source() {
-		checkOutvalue(200);
-		return "Greetings from source sell!";
-	}
+    @GetMapping("/api/v1/sell/source")
+    public String source() {
+        checkOutvalue(200);
+        return "Greetings from source sell!";
+    }
 
-	@GetMapping("/api/v1/sell/2")
-	public String sell2() {
-		return "Greetings from sell2!";
-	}
+    @GetMapping("/api/v1/sell/2")
+    public String sell2() {
+        return "Greetings from sell2!";
+    }
 
-	@GetMapping("/api/v1/error")
-	public String error() throws Exception {
-		throw new Exception("error processing");
-	}
+    @GetMapping("/api/v1/error")
+    public String error() throws Exception {
+        throw new Exception("error processing");
+    }
 
-	private void checkOutvalue(int total){
-		System.out.println("Check out value " + total);
-	}
+    private void checkOutvalue(int total) {
+        System.out.println("Check out value " + total);
+    }
 
-	@GetMapping("/api/v0")
-	public main.java.com.example.springboot.UserResponse restResponse() {
-		main.java.com.example.springboot.UserResponse userResponse = new main.java.com.example.springboot.UserResponse();
+    void checkOutvalue2(int total) {
+        System.out.println("Check out value " + total);
+    }
 
-		userResponse.setStatus(true);
-		userResponse.setMessage("Data is found");
+    @GetMapping("/api/v0")
+    public main.java.com.example.springboot.UserResponse restResponse() {
+        main.java.com.example.springboot.UserResponse userResponse = new main.java.com.example.springboot.UserResponse();
 
-		return userResponse;
-	}
+        userResponse.setStatus(true);
+        userResponse.setMessage("Data is found");
+
+        return userResponse;
+    }
 
 }
 
